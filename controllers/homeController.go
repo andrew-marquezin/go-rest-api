52e05205486f3644993b5f8bcadf8d2d2e4dc7cc
andrew-marquezin <andrewmmarinho@gmail.com>
2024-10-21 16:11:39 -0300

    preparação para começar a usar um banco de dados

--- controllers/homeController.go
+++ controllers/homeController.go
@@ -5,12 +5,26 @@ import (
 	"fmt"
 	"go-rest-api/models"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Home page")
 }
 
-func AllPersonalities(w http.ResponseWriter, r *http.Request) {
+func AllPersonas(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(models.Personalidades)
+}
+
+func OnePersona(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	for _, persona := range models.Personalidades {
+		if strconv.Itoa(persona.Id) == id {
+			json.NewEncoder(w).Encode(persona)
+		}
+	}
 }
