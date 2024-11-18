45e6de358b58358c257f0b213236028219fc19fd
andrew-marquezin <andrewmmarinho@gmail.com>
2024-11-18 16:17:53 -0300

    adicionado o moddleware de content type

--- routes/routes.go
+++ routes/routes.go
@@ -2,19 +2,22 @@ package routes
 
 import (
 	"go-rest-api/controllers"
+	"go-rest-api/middlewares"
 	"log"
 	"net/http"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
 func HandleRequest() {
 	r := mux.NewRouter()
+	r.Use(middlewares.ContentTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.AllPersonas).Methods("Get")
 	r.HandleFunc("/api/personalidades/{id}", controllers.OnePersona).Methods("Get")
 	r.HandleFunc("/api/personalidades", controllers.CreateNewPersona).Methods("Post")
 	r.HandleFunc("/api/personalidades/{id}", controllers.DeletePersona).Methods("Delete")
 	r.HandleFunc("/api/personalidades/{id}", controllers.EditPersona).Methods("Put")
-	log.Fatal(http.ListenAndServe(":8000", r))
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedOrigins([]string{"http://localhost:3000"}))(r)))
 }
